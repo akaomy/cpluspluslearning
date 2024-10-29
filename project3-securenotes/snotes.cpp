e3155896933fe45f1ac64a3bf5fe70456be87783
akaomy <annakozlova92@gmail.com>
2024-10-29 15:38:22 -0700

    add encrypt and decrypt data

--- project3-securenotes/snotes.cpp
+++ project3-securenotes/snotes.cpp
@@ -46,6 +46,58 @@ EVP_PKEY *generate_key_pair() {
 
 }
 
+bool encrypt(const unsigned char *plaintext, int plaintext_len, 
+    const unsigned char *key, const unsigned char *iv, 
+    unsigned char *ciphertext, int &ciphertext_len) {
+    
+    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+    if (!ctx) {
+        std::cerr << "Failed to create EVP_CIPHER_CTX" << std::endl;
+        return false;
+    }
+
+    if (EVP_EncryptInit_ex(ctx, EVP_aes_256_gcm(), nullptr, key, iv) != 1) {
+        std::cerr << "Failed to initialize encryption" << std::endl;
+        EVP_CIPHER_CTX_free(ctx);
+        return false;
+    }
+
+    if (EVP_EncryptUpdate(ctx, ciphertext, &ciphertext_len, 
+        plaintext, plaintext_len) != 1) {
+        std::cerr << "Failed to encrypt data" << std::endl;
+        EVP_CIPHER_CTX_free(ctx);
+        return false;
+    }
+
+    EVP_CIPHER_CTX_free(ctx);
+    return true;
+}
+
+bool decrypt(const unsigned char *ciphertext, int ciphertext_len, 
+    const unsigned char *key, const unsigned char *iv, 
+    unsigned char *plaintext, int &plaintext_len) {
+    
+    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+    if (!ctx) {
+        std::cerr << "Failed to create EVP_CIPHER_CTX" << std::endl;
+        return false;
+    }
+
+    if (EVP_DecryptInit_ex(ctx, EVP_aes_256_gcm(), nullptr, key, iv) != 1) {
+        std::cerr << "Failed to initialize decryption" << std::endl;
+        EVP_CIPHER_CTX_free(ctx);
+        return false;
+    }
+
+    if (EVP_DecryptUpdate(ctx, plaintext, &plaintext_len, ciphertext, ciphertext_len) != 1) {
+        std::cerr << "Failed to decrypt data" << std::endl;
+        EVP_CIPHER_CTX_free(ctx);
+        return false;
+    }
+
+    EVP_CIPHER_CTX_free(ctx);
+    return true;
+}
 int main() {
     string title, content;
 
