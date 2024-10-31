ce2d167a6508d3086cd34d3acad7d39d5eb1c044
akaomy <annakozlova92@gmail.com>
2024-10-31 14:39:13 -0700

    add examples of some of the runtime errors

--- c++ general/errors.cpp
+++ c++ general/errors.cpp
@@ -1,3 +1,21 @@
-//
-// Created by Anna on 10/31/2024.
-//
+#include <iostream>
+// 2. link-time error
+// #include <somefile.h>
+
+using namespace std;
+
+int main()
+{
+  // 1. compile-time error:
+  // Int a, b, c;
+
+  // 3. type errors
+  // cout << 10 / 0;
+
+  // 4. logic errors
+  // programmer related problems
+
+  cout << "Hello World!" << endl;
+  return 0;
+}
+
