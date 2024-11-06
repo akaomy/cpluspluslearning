b768069ba7a083e0e8ccf00646bb7d4b88f4d525
akaomy <annakozlova92@gmail.com>
2024-11-06 10:29:49 -0800

    add constructor, rewrite node creation statements to use generic type class Node, reflect these changes for PrintNode function parameter

--- dsa/Node/Node.cpp
+++ dsa/Node/Node.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-void PrintNode(Node *node) {
+void PrintNode(const Node<float> *node) {
   // print out initial node
   // until it find NULL for the Next pointer
   // meaning end of the node chain
@@ -16,14 +16,9 @@ void PrintNode(Node *node) {
 };
 
 int main() {
-  const auto first_node = new Node;
-  first_node->value = 1;
-
-  const auto second_node = new Node;
-  second_node->value = 2;
-
-  const auto third_node = new Node;
-  third_node->value = 3;
+  const auto first_node = new Node<float>(1.23);
+  const auto second_node = new Node<float>(3.56);
+  const auto third_node = new Node<float>(7.89);
 
   first_node->next = second_node;
   second_node->next = third_node;
