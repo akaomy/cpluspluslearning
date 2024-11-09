05d46e4b059d54b43eb017d7ecdeacca44b291a6
akaomy <annakozlova92@gmail.com>
2024-11-09 10:53:49 -0800

    add more data basic types and display their content

--- dsa/basics/basicds.cpp
+++ dsa/basics/basicds.cpp
@@ -1,25 +1,37 @@
 #include <iostream>
 
+using namespace std;
+
+void voidFunc()
+{
+    cout << "from void function" << endl;
+};
+
 int main() {
     int i; // variable definition or default initialization
     int iVar = 0; // copy initialization
-    int oVar(100); // direct initialisation 
-    int jVar{100}; // uniform intialization
+    int oVar(100); // direct initialisation
+    int jVar{100}; // uniform intialization - TODO: learn more about it
     int uVar{}; // value initialisation
 
-    std::cout << "Please enter an integer value: ";
-    std::cin >> i;
-    std::cout << "The value you entered is: " << i;
-    std::cout << "\n";
+    bool aVar{true};
+    char bVar{'c'};
+    float cVar{3.14};
+    int dVar{10};
+    voidFunc();
 
+    cout << "int i => " << i << endl;
+    cout << "int iVar => " << iVar << endl;
+    cout << "oVar(100) => " << oVar << endl;
+    cout << "jVar{100} => " << jVar << endl;
+    cout << "uVar{} => " << uVar << endl;
 
-
-    // control flow
-    if (i >= 0 && i < 100) {
-        std::cout << "less than 100" << std::endl;
-    } else {
-        std::cout << "greater than or equal to 100" << std::endl;
-    }
+    cout << "bool aVar{true} => " << aVar << endl;
+    cout << "bool bVar{'c'} => " << bVar << endl;
+    cout << "bool cVar{3.14} => " << cVar << endl;
+    cout << "bool dVar{10} => " << dVar << endl;
+    voidFunc();
+    cout << "bool voidFunc() => " << endl;
 
     return 0;
 }
