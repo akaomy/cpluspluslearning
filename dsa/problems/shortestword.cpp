813e708f8b12e1f3ee7594d5199f503d3be52eb9
akaomy <annakozlova92@gmail.com>
2024-11-11 01:22:21 -0800

    add problem - find shortest word

--- dsa/problems/shortestword.cpp
+++ dsa/problems/shortestword.cpp
@@ -1,26 +1,54 @@
 #include <iostream>
+#include <string.h>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-  // char str[] = "Fundamentals of Data Engineering.";
-  // using no built-in function
-  // to separate string into words
+  // task: return length of the shortest word in given string
+  // it will never be empty, and you don't need to account for different data types
 
-  char word[] = " ";
-  word[0] = 'a';
-  word[1] = 'b';
-  word[2] = 'c';
-  cout << word[0] << word[1] << word[2] << endl;
-  // for (int i = 0; i < 34; i++)
-  // {
-  //   if (str[i] == ' ')
-  //   {
-  //     // str[i] = ',';
-  //     word = word + str[i];
-  //   }
-  //   cout << word << endl;
-  // }
+  // pseudocode:
+  // 1. construct vector of words
+    // go through each character in the given string
+    // put each character into the string word except it's punctiation until it hits white space
+    // if white space - push accumulated string word into vector words
+  // 2. look for shortest word in words
+    // initialize the shortest_word string to the first word in the vector of words
+    // go through vector of words
+    // compare current word length with the shortest_word
+    // if current is shorter, current becomes shortest
+  string str = "Let's we're Test Fundamentals of Data Engineering.";
+  vector<string> words;
+  string word;
+
+  for (auto chr : str)
+  {
+    if (chr == ' ')
+    {
+      words.push_back(word);
+      word.clear(); // clear the word to start collecting the next one
+    }
+    else
+    {
+      if (!ispunct(chr))
+      {
+        word += chr;
+      }
+    }
+  }
+  words.push_back(word); // add last word in
+
+  string shortestWord = words[0];
+  for (auto current_word : words)
+  {
+    if (current_word.length() < shortestWord.length())
+    {
+      shortestWord = current_word;
+    }
+  }
+  cout << "your shortest string: " << shortestWord << endl;
+
   return 0;
 }
