995abe18d32fd469588c30487f80592229cdd840
akaomy <annakozlova92@gmail.com>
2024-11-15 23:13:09 -0800

    fix logic to add nested loops and check if two numbers are distinct O(n^2) solution

--- dsa/problems/easy/addUpToZero.cpp
+++ dsa/problems/easy/addUpToZero.cpp
@@ -5,21 +5,18 @@
 bool checkForSumZero(const std::array<int, 6>& arr)
 {
     // Given a list of ints, return True if any two numbers in the list add up to 0.
-    int initNum=arr.at(0);
-
-    for (int i = 0; i < 6; i++)
+    for (int i = 0; i < arr.size(); i++)
     {
-        initNum += i;
-        if (initNum == 0)
+        for (int j = 0; j < arr.size(); j++)
         {
-            std::cout << "string: True" << std::endl;
-            return true;
-        }
-        else
-        {
-            initNum = arr.at(0);
+            if (arr[i] != arr[j] && arr[i] + arr[j] == 0)
+            {
+                std::cout << "True" << std::endl;
+                return true;
+            }
         }
     }
+    std::cout << "False" << std::endl;
     return false;
 }
 
@@ -28,6 +25,7 @@ void test_checkForSumZero()
     std::array<int, 6> test1 = {4, -4, 3, -3, 2, 6};
     std::array<int, 6> test2 = {1, 2, 3, 4, 5, 6};
     std::array<int, 6> test3 = {0, 1, 2, 3, 4, 5};
+
 
     // case1: expect true
     assert(checkForSumZero(test1) == true);
