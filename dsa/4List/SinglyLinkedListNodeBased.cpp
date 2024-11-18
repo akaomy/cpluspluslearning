b1c7b111f8d97aa7ad79167a9b45c57b83a5b1ed
akaomy <annakozlova92@gmail.com>
2024-11-17 21:10:47 -0800

    refactor folder structure for clarity, add a few initial .cpp files for future use for Containers folder

--- dsa/4List/SinglyLinkedListNodeBased.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Anna on 11/6/2024.
-//
-
-#include "SinglyLinkedListNodeBased.h"
---
+++ dsa/problems/easy/TODOmapForWordsAndNums.cpp
@@ -0,0 +1,2 @@
+//Given a string, return a dictionary where the keys are the length of the words in the string and the
+// values are the number of words of that length (“hello my name is sam” => { 4: 1, 2: 2, 3: 1, 4: 1} )
