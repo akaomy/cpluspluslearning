944644a47093de0ccb48e7a0908ec024c3160516
akaomy <annakozlova92@gmail.com>
2024-11-18 14:38:27 -0800

    add insertAtEnd() and printList() that traverses through the linked list member functions

--- dsa/3List/LinkedList.h
+++ dsa/3List/LinkedList.h
@@ -6,26 +6,77 @@
 template <typename T>
 class LinkedList {
     public:
-      Node<T>* head;
-      Node<T>* tail;
+        Node<T>* head;
+        Node<T>* tail;
 
-      // constructor
-      LinkedList() : head(nullptr), tail(nullptr) {};
+        // constructor
+        LinkedList() : head(nullptr), tail(nullptr) {};
 
-      // destructor
-      ~LinkedList() {};
+        // destructor
+        ~LinkedList() {};
 
-      // get
-      Node<T>* getHead() const
-      {
-        return head;
-      };
+        // get
+        T getHead() const
+        {
+            if (head != nullptr)
+            {
+                return head->value;
+            }
+            return T(); // Return default value (0 for int, nullptr for pointer types, etc.)
+        };
 
-      Node<T>* getTail() const
-      {
-        return tail;
-      };
-      // insert
+        T getTail() const
+        {
+            if (tail != nullptr)
+            {
+                return tail->value;
+            }
+            return T(); // Return default value (0 for int, nullptr for pointer types, etc.)
+        };
+
+        // insert
+        int insertAtEnd(T nodeValue)
+        {
+            // create a new node with some vals
+            auto *newNode = new Node<T>(nodeValue);
+            // check if the LinkedList is empty
+            if (head == nullptr && tail == nullptr)
+            {
+                head = newNode;
+                tail = newNode;
+            // check if there is at least one element present
+            }
+            else
+            {
+                // current last node connects to newNode
+                tail->next = newNode;
+                tail = newNode;
+            }
+            return 1;
+        };
+
+    void printList()
+    {
+        Node<T> *current = head;
+        // traverse the list and print each node value and next pointer
+        while (current != nullptr)
+        {
+            std::cout << "Node current value: " << current->value << std::endl;
+            if (current->next != nullptr)
+            {
+                std::cout << "Node current next value " << current->next->value << std::endl;
+            }
+            else
+            {
+                std::cout << "Node current next nullptr" << std::endl;
+            }
+
+            std::cout << std::endl;
+            current = current->next;
+        }
+    }
+
+
           // beginnig
           // end
           // any position between beg. and end
