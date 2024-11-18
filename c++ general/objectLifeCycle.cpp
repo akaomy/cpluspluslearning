82f9f930d5d144e7e5a2d3567dfb2bb24d4a5fa2
akaomy <annakozlova92@gmail.com>
2024-11-17 19:41:29 -0800

    add note .cpp file on object life cycle

---
+++ c++ general/objectLifeCycle.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+class Entity
+{
+  public:
+  Entity()
+  {
+    std::cout << "Entity created" << std::endl;
+  };
+  ~Entity()
+  {
+    std::cout << "Entity deleted" << std::endl;
+  }
+};
+void foo()
+{
+  int a = 0;
+  std::cout << a << std::endl;
+  // entity one activates destructor only when program execution will real end of function foo scope
+  Entity one;
+  Entity* two = new Entity();
+}
+
+
+int main() {
+  // stack based:
+  // will be initialized when it is called and executed
+  // as soon as gets out of scope variable or object is getting destoyed to free up memory
+
+  foo();
+  // Entity e;
+
+  // Heap based:
+  // only created during program run. it doesn't get destroyed if it goes out of scope
+  // only gets destroyed when programm stops its execution
+  // Entity* s = new Entity();
+
+  return 0;
+}
