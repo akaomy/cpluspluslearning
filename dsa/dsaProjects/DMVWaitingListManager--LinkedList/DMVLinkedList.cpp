517e3f6169ab49938fcc1933025f2cbca71bfedd
akaomy <annakozlova92@gmail.com>
2024-11-22 16:08:15 -0800

    test my Linked List

--- dsa/dsaProjects/DMVWaitingListManager--LinkedList/DMVLinkedList.cpp
+++ dsa/dsaProjects/DMVWaitingListManager--LinkedList/DMVLinkedList.cpp
@@ -1,5 +1,23 @@
-//
-// Created by Anna on 11/22/2024.
-//
-
 #include "DMVLinkedList.h"
+#include <iostream>
+#include <vector>
+
+
+int main()
+{
+  DMVLinkedList waitingList;
+  int amountOfPeople = 5;
+
+  for (int i = 0; i < amountOfPeople; i++)
+  {
+    // temp for testing purposes
+    std::string name = "Person " + std::to_string(i+1);
+    std::string time = "10:" + std::to_string(30+i) + "AM";
+
+    waitingList.addPersonToTheEnd(name, time);
+  }
+
+  waitingList.displayWaitlist();
+
+  return 0;
+}
