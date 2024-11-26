060fc7b25c764d07debac4c931a8daf2556c4dbe
akaomy <annakozlova92@gmail.com>
2024-11-25 21:01:05 -0800

    in process of solving flippingMatrix problem

---
+++ dsa/problems/hr/fizzbuzz.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Anna on 11/25/2024.
+//
---
+++ dsa/problems/hr/flippingMatrix.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+  // input: 2 dimentional matrix with int
+  // output: max sum of the matrix
+
+  return 0;
+}
