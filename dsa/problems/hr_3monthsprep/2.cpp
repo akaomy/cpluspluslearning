a09af6a48c12440983c865bc72b9ce248617038c
akaomy <annakozlova92@gmail.com>
2024-11-27 14:34:20 -0800

    add solution for second hr problem for 3 month prep track

---
+++ dsa/problems/hr_3monthsprep/2.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <limits> // safe and portable way to define largest possible vals for a type
+
+int main()
+{
+  auto *arr = new double[5]{0, 0, 0, 0, 0};
+  // simulating input
+  // 1. save input ints into an array (since there is set amount of ints)
+  std::cout << "Enter number: " << std::endl;
+  for (int i = 0; i < 5; i++)
+  {
+    std::cin >> arr[i];
+  }
+
+  // auto to account big numbers
+  // limitation: 1 <= arr[i] <= 10^9
+  long long max = 0;
+  long long min = std::numeric_limits<long long>::max(); // limitation?
+  // here should be check for empty array
+
+  for (int i = 0; i < 5; i++)
+  {
+    long long sum = 0;
+    // first iteration sum all numbers exept the last one
+    // second iteration sum all numbs but second to last
+    // third iteration sum all nums but the middle one
+    // fourth iteration sum all nums but second elem
+    // fith iteration sum all nums but first one
+    // calculate sum excluding i-th element
+    for (int j = 0; j < 5; j++) {
+      if (j != i) {
+        // skip i-th element
+        sum += arr[j];
+      }
+    }
+    if (sum > max) {
+      max = sum;
+    }
+    if (sum < min) {
+      min = sum;
+    }
+
+  }
+  std::cout << min << " " << max <<  std::endl;
+
+  return 0;
+}
